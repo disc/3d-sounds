ce940fe26cdcade001f9cd150bd9024ec644ef02
Aleksandr Khachikyants <disc@mydbg.ru>
2013-01-11 00:27:39 +0200

    добавил help активити, модемный звук и локализацию на русский язык

---
+++ src/com/example/discTest/HelpActivity.java
@@ -0,0 +1,11 @@
+package com.example.discTest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelpActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.help);
+    }
+}
--- src/com/example/discTest/MainActivity.java
+++ src/com/example/discTest/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.discTest;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
     /**
@@ -11,5 +15,20 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    public void firstSound(View v) {
+        final MediaPlayer mp1 = MediaPlayer.create(getApplicationContext(), R.raw.modem_sound);
+        mp1.start();
+
+        CharSequence text = getApplicationContext().getResources().getText(R.string.first_sound_toast);
+
+        Toast toast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    public void showAboutActivity(View v) {
+        Intent myIntent = new Intent(getApplicationContext(), HelpActivity.class);
+        startActivityForResult(myIntent, 0);
     }
 }
