8164c49c8e2053a2952845e14101eb9e5264f326
Aleksandr Khachikyants <disc@mydbg.ru>
2013-01-15 02:04:25 +0200

    сделал динамичные селекторы для элементов списка переделал изменения стилей предыдущего элемента

--- src/ru/disc/sounds3d/MainActivity.java
+++ src/ru/disc/sounds3d/MainActivity.java
@@ -3,23 +3,24 @@ package ru.disc.sounds3d;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.res.AssetFileDescriptor;
+import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.AdapterView;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.*;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends Activity {
-    MediaPlayer mediaPlayer;
+    private MediaPlayer mediaPlayer = new MediaPlayer();
+    private int currentPosition = 0;
+    private boolean isPaused = false;
 
     /**
      * Список элементов вида ключ-значение, где ключ - название трека, значение - путь к файлу
@@ -41,102 +42,154 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+        // работа со списком
         ListView listView = (ListView)findViewById(R.id.listView);
 
+        // заполняю список элементами из массива
         MySimpleArrayAdapter adapter = new MySimpleArrayAdapter(getApplicationContext(), elements.keySet().toArray());
         listView.setAdapter(adapter);
 
+        // обработчик нажатия по элементу списка
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                //убираю иконку паузы у всех элементов
-                setPlayIconForAllElementsInList(parent);
-
-                try {
-                    // получаю нажатый элемент и меняю иконку
-                    ImageView imageView = (ImageView)view.findViewById(R.id.icon);
-                    imageView.setImageResource(R.drawable.ic_media_pause);
-                } catch (Exception e){
-                    Toast.makeText(getApplicationContext(), "Could not find element.", Toast.LENGTH_LONG).show();
-                }
-
-                // проигрываю звук
-                mediaPlayer.reset();
-                try {
-                    Integer fileResource = (Integer)elements.values().toArray()[position];
-                    AssetFileDescriptor afd = getResources().openRawResourceFd(fileResource);
-                    if (afd == null) {
-                        Toast.makeText(getApplicationContext(), "Could not load sound.", Toast.LENGTH_LONG).show();
-                        return;
-                    }
-                    mediaPlayer.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
-                    afd.close();
-                    mediaPlayer.prepare();
-                    mediaPlayer.start();
-                } catch (Exception e) {
-                    Toast.makeText(getApplicationContext(), "Could not load sound.", Toast.LENGTH_LONG).show();
-                }
+                currentPosition = position;
+                Log.d("Test", "Position: " + position);
+                playSong(position);
             }
         });
+    }
 
-        // событие, происходящее после того как медиа-плеер закончил играть трек
-        MediaPlayer.OnCompletionListener completionListener  = new MediaPlayer.OnCompletionListener(){
-            @Override
-            public void onCompletion(MediaPlayer arg0) {
-                ListView listView = (ListView)findViewById(R.id.listView);
-                //TODO: убрать этот вызов и сделать смену иконки только для текущего трека который играет
-                setPlayIconForAllElementsInList(listView);
+    private void changeBackgroundForListElement(int position) {
+        try {
+            // получаю нажатый элемент и меняю иконку
+            ListView listView = (ListView)findViewById(R.id.listView);
+            // меняю фон и иконку для текущего проигрываемого элемента
+            View view = listView.getChildAt(position);
+            TextView elemTitle = (TextView)view.findViewById(R.id.elTitle);
+            elemTitle.setTextColor(Color.BLACK);
+            elemTitle.setBackgroundResource(R.drawable.list_item_act);
+            ImageView imageView = (ImageView)view.findViewById(R.id.icon);
+            imageView.setImageResource(R.drawable.ic_media_pause);
+
+            if (position > 0) {
+                int prevElementPos = position  - 1;
+                // меняю фон и иконку для предыдущего
+                View viewPrev = listView.getChildAt(prevElementPos);
+                TextView elemTitlePrev = (TextView)viewPrev.findViewById(R.id.elTitle);
+                elemTitlePrev.setTextColor(Color.WHITE);
+                elemTitlePrev.setBackgroundResource(R.drawable.list_item);
+                ImageView imageViewPrev = (ImageView)viewPrev.findViewById(R.id.icon);
+                imageViewPrev.setImageResource(R.drawable.ic_media_play);
             }
-        };
-
-        mediaPlayer = new MediaPlayer();
-        mediaPlayer.setOnCompletionListener(completionListener);
-    }
-
-    private void setPlayIconForAllElementsInList(AdapterView<?> parent) {
-        ListView lv = (ListView)parent.findViewById(R.id.listView);
-        ArrayList<View> arrayListView = lv.getTouchables();
-        for(View v: arrayListView){
-            ImageView imageView = (ImageView)v.findViewById(R.id.icon);
-            imageView.setImageResource(R.drawable.ic_media_play);
+        } catch (Exception e){
+            Toast.makeText(getApplicationContext(), "Could not find element.", Toast.LENGTH_LONG).show();
         }
     }
 
-    @Override
-    public void onAttachedToWindow() {
-        super.onAttachedToWindow();
-        this.getWindow().setType(WindowManager.LayoutParams.TYPE_KEYGUARD);
+    /**
+     * Обработчик кнопки "Next"
+     * @param v
+     */
+    public void nextTrackButtonClick(View v){
+        nextSong();
     }
 
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        switch (keyCode) {
-            case KeyEvent.KEYCODE_BACK:
-                Log.d("Test", "Back button pressed!");
-                mediaPlayer.pause();
-                break;
-            case KeyEvent.KEYCODE_HOME:
-                Log.d("Test", "Home button pressed!");
-                finish();
-                break;
+    /**
+     * Обработчик кнопки "Prev"
+     * @param v
+     */
+    public void prevTrackButtonClick(View v){
+        playSong(currentPosition > 0 ? --currentPosition : currentPosition);
+    }
+
+    /**
+     * Обработчик кнопки "Play"
+     * @param v
+     */
+    public void playButtonClick(View v){
+        if (mediaPlayer.isPlaying()) {
+            isPaused = true;
+            mediaPlayer.pause();
+        } else if (isPaused) {
+            mediaPlayer.seekTo(mediaPlayer.getCurrentPosition());
+            mediaPlayer.start();
+        } else {
+             playSong(currentPosition);
         }
-        return super.onKeyDown(keyCode, event);
     }
 
-    @Override
-    protected void onStop() {
-        Log.d("Test", "Home button pressed!");
-        super.onStop();
+    private void playSong(Integer trackNumber) {
+        changeBackgroundForListElement(trackNumber);
+
+        try {
+            mediaPlayer.reset();
+            Integer fileResource = (Integer)elements.values().toArray()[trackNumber];
+            AssetFileDescriptor afd = getResources().openRawResourceFd(fileResource);
+            if (afd == null) {
+                Toast.makeText(getApplicationContext(), "Could not load sound.", Toast.LENGTH_LONG).show();
+                return;
+            }
+            mediaPlayer.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
+            afd.close();
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+
+            // событие, происходящее после того как медиа-плеер закончил играть трек
+            mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                public void onCompletion(MediaPlayer arg0) {
+                    nextSong();
+                }
+            });
+        } catch (IOException e) {
+            Log.v(getString(R.string.app_name), e.getMessage());
+        }
+    }
+
+    private void nextSong() {
+        if (++currentPosition >= elements.size()) {
+            // если конец списка начинаю сначала
+            //TODO: добавить проверку что включен repeat
+            currentPosition = 0;
+        }
+        playSong(currentPosition);
     }
 
 //    @Override
-//    protected void onPause() {
-//        super.onPause();
-//        mediaPlayer.pause();
+//    public void onAttachedToWindow() {
+//        super.onAttachedToWindow();
+//        this.getWindow().setType(WindowManager.LayoutParams.TYPE_KEYGUARD);
 //    }
 
-    public void showAboutActivity(View v) {
-        Intent myIntent = new Intent(getApplicationContext(), HelpActivity.class);
-        startActivityForResult(myIntent, 0);
+//    @Override
+//    public boolean onKeyDown(int keyCode, KeyEvent event) {
+//        switch (keyCode) {
+//            case KeyEvent.KEYCODE_BACK:
+//                Log.d("Test", "Back button pressed!");
+//                mediaPlayer.pause();
+//                break;
+//            case KeyEvent.KEYCODE_HOME:
+//                Log.d("Test", "Home button pressed!");
+//                finish();
+//                break;
+//        }
+//        return super.onKeyDown(keyCode, event);
+//    }
+//
+//    @Override
+//    protected void onStop() {
+//        Log.d("Test", "Home button pressed!");
+//        super.onStop();
+//    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mediaPlayer.pause();
     }
+
+//    public void showAboutActivity(View v) {
+//        Intent myIntent = new Intent(getApplicationContext(), HelpActivity.class);
+//        startActivityForResult(myIntent, 0);
+//    }
 }
